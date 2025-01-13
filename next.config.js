f8094e97bafdd7b7ce836b0926d5ea749d291fba
Mear29 <46094742+Mear29@users.noreply.github.com>
2025-01-14 00:48:42 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -8,7 +8,7 @@ module.exports = {
     return [
       {
         source: '/safe-image.jpg',
-        destination: 'http://127.0.0.2/?p=../admin/post&id=4',
+        destination: 'http://127.0.0.2/?p=../admin/post&id=5',
         permanent: true,
       },
     ];
